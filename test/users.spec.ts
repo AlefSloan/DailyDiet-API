6d7466691860f8756cf1beaf523c5e8cc8bfd403
alefsloan <alef_sloan@hotmail.com>
2023-10-17 02:22:05 -0300

    test: test list users and get specific users case

--- test/users.spec.ts
+++ test/users.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, beforeAll, afterAll, beforeEach } from 'vitest'
+import { describe, it, beforeAll, afterAll, beforeEach, expect } from 'vitest'
 import { execSync } from 'child_process'
 import request from 'supertest'
 import { app } from '../src/app'
@@ -20,13 +20,45 @@ describe('Users Route', () => {
     await request(app.server)
       .post('/users')
       .send({
-        email: 'johndoe@hotmail.com',
+        email: 'johndoe@example.com',
         password: 'dailydiet',
       })
       .expect(201)
   })
 
-  it('', async () => {})
+  it('Should be able to list all user', async () => {
+    await request(app.server).post('/users').send({
+      email: 'johndoe@example.com',
+      password: 'dailydiet',
+    })
 
-  it('', async () => {})
+    const listUsersResponse = await request(app.server).get('/users')
+
+    expect(listUsersResponse.body.users).toEqual([
+      expect.objectContaining({
+        email: 'johndoe@example.com',
+      }),
+    ])
+  })
+
+  it('Should be able to get a specific user', async () => {
+    await request(app.server).post('/users').send({
+      email: 'johndoe@example.com',
+      password: 'dailydiet',
+    })
+
+    const listUsersResponse = await request(app.server).get('/users')
+
+    const createdUserId = listUsersResponse.body.users[0].id
+
+    const getUserResponse = await request(app.server).get(
+      `/users/${createdUserId}`,
+    )
+
+    expect(getUserResponse.body.user).toEqual(
+      expect.objectContaining({
+        email: 'johndoe@example.com',
+      }),
+    )
+  })
 })
