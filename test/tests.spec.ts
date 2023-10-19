ed9af72ea5756c077f76be09902a26b93cd1fb7b
alefsloan <alef_sloan@hotmail.com>
2023-10-18 21:52:46 -0300

    test: create login route tests

---
+++ test/tests.spec.ts
@@ -0,0 +1,98 @@
+import { describe, it, beforeAll, afterAll, beforeEach, expect } from 'vitest'
+import { execSync } from 'child_process'
+import request from 'supertest'
+import { app } from '../src/app'
+
+describe('', () => {
+  describe('Users Route', () => {
+    beforeAll(async () => {
+      app.ready()
+    })
+
+    afterAll(async () => {
+      app.close()
+    })
+
+    beforeEach(() => {
+      execSync('npm run resetdb')
+    })
+
+    it('Should create a new user', async () => {
+      await request(app.server)
+        .post('/users')
+        .send({
+          email: 'johndoe@example.com',
+          password: 'dailydiet',
+        })
+        .expect(201)
+    })
+
+    it('Should be able to list all user', async () => {
+      await request(app.server).post('/users').send({
+        email: 'johndoe@example.com',
+        password: 'dailydiet',
+      })
+
+      const listUsersResponse = await request(app.server).get('/users')
+
+      expect(listUsersResponse.body.users).toEqual([
+        expect.objectContaining({
+          email: 'johndoe@example.com',
+        }),
+      ])
+    })
+
+    it('Should be able to get a specific user', async () => {
+      await request(app.server).post('/users').send({
+        email: 'johndoe@example.com',
+        password: 'dailydiet',
+      })
+
+      const listUsersResponse = await request(app.server).get('/users')
+
+      const createdUserId = listUsersResponse.body.users[0].id
+
+      const getUserResponse = await request(app.server).get(
+        `/users/${createdUserId}`,
+      )
+
+      expect(getUserResponse.body.user).toEqual(
+        expect.objectContaining({
+          email: 'johndoe@example.com',
+        }),
+      )
+    })
+  })
+
+  describe('Login Route', () => {
+    beforeAll(async () => {
+      app.ready()
+    })
+
+    afterAll(async () => {
+      app.close()
+    })
+
+    beforeEach(() => {
+      execSync('npm run resetdb')
+    })
+
+    it('Should login when given a correct email and password', async () => {
+      await request(app.server)
+        .post('/users')
+        .send({
+          email: 'johndoe@example.com',
+          password: 'dailydiet',
+        })
+        .expect(201)
+
+      await request(app.server)
+        .post('/login')
+        .send({
+          email: 'johndoe@example.com',
+          password: 'dailydiet',
+        })
+        .expect(200)
+    })
+  })
+})
