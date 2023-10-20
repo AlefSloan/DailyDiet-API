aadc2f10c6415966c32c4690fb8e5c19cae5a418
alefsloan <alef_sloan@hotmail.com>
2023-10-19 21:46:58 -0300

    test: create tests for meals route

--- test/tests.spec.ts
+++ test/tests.spec.ts
@@ -4,19 +4,19 @@ import request from 'supertest'
 import { app } from '../src/app'
 
 describe('', () => {
+  beforeAll(async () => {
+    app.ready()
+  })
+
+  afterAll(async () => {
+    app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run resetdb')
+  })
+
   describe('Users Route', () => {
-    beforeAll(async () => {
-      app.ready()
-    })
-
-    afterAll(async () => {
-      app.close()
-    })
-
-    beforeEach(() => {
-      execSync('npm run resetdb')
-    })
-
     it('Should create a new user', async () => {
       await request(app.server)
         .post('/users')
@@ -65,18 +65,6 @@ describe('', () => {
   })
 
   describe('Login Route', () => {
-    beforeAll(async () => {
-      app.ready()
-    })
-
-    afterAll(async () => {
-      app.close()
-    })
-
-    beforeEach(() => {
-      execSync('npm run resetdb')
-    })
-
     it('Should login when given a correct email and password', async () => {
       await request(app.server)
         .post('/users')
@@ -93,6 +81,194 @@ describe('', () => {
           password: 'dailydiet',
         })
         .expect(200)
+    })
+  })
+
+  describe('Meals Route', () => {
+    it('Should create a meal', async () => {
+      await request(app.server).post('/users').send({
+        email: 'johndoe@example.com',
+        password: 'dailydiet',
+      })
+
+      const loginResponse = await request(app.server).post('/login').send({
+        email: 'johndoe@example.com',
+        password: 'dailydiet',
+      })
+
+      const cookies = loginResponse.get('Set-Cookie')
+
+      await request(app.server)
+        .post('/meals')
+        .set('Cookie', cookies)
+        .send({
+          name: 'X-tudo',
+          description: 'X-tudo da esquina',
+          dateTime: '2023-10-17T08:30:00Z',
+          isDiet: false,
+        })
+        .expect(201)
+    })
+
+    it('Should be able to list meals', async () => {
+      await request(app.server).post('/users').send({
+        email: 'johndoe@example.com',
+        password: 'dailydiet',
+      })
+
+      const loginResponse = await request(app.server).post('/login').send({
+        email: 'johndoe@example.com',
+        password: 'dailydiet',
+      })
+
+      const cookies = loginResponse.get('Set-Cookie')
+
+      await request(app.server).post('/meals').set('Cookie', cookies).send({
+        name: 'X-tudo',
+        description: 'X-tudo da esquina',
+        dateTime: '2023-10-17T08:30:00Z',
+        isDiet: false,
+      })
+
+      const mealsListResponse = await request(app.server)
+        .get('/meals')
+        .set('Cookie', cookies)
+
+      expect(mealsListResponse.body.meals).toEqual([
+        expect.objectContaining({
+          name: 'X-tudo',
+          description: 'X-tudo da esquina',
+          meal_datetime: '2023-10-17T08:30:00Z',
+          is_diet: 0,
+        }),
+      ])
+    })
+
+    it('Should be able to get a specific meal', async () => {
+      await request(app.server).post('/users').send({
+        email: 'johndoe@example.com',
+        password: 'dailydiet',
+      })
+
+      const loginResponse = await request(app.server).post('/login').send({
+        email: 'johndoe@example.com',
+        password: 'dailydiet',
+      })
+
+      const cookies = loginResponse.get('Set-Cookie')
+
+      await request(app.server).post('/meals').set('Cookie', cookies).send({
+        name: 'X-tudo',
+        description: 'X-tudo da esquina',
+        dateTime: '2023-10-17T08:30:00Z',
+        isDiet: false,
+      })
+
+      const mealsListResponse = await request(app.server)
+        .get('/meals')
+        .set('Cookie', cookies)
+
+      const mealId = mealsListResponse.body.meals[0].id
+
+      const getMealsResponse = await request(app.server)
+        .get(`/meals/${mealId}`)
+        .set('Cookie', cookies)
+
+      expect(getMealsResponse.body.meals).toEqual(
+        expect.objectContaining({
+          name: 'X-tudo',
+          description: 'X-tudo da esquina',
+          meal_datetime: '2023-10-17T08:30:00Z',
+          is_diet: 0,
+        }),
+      )
+    })
+
+    it('Should be able to update a meal', async () => {
+      await request(app.server).post('/users').send({
+        email: 'johndoe@example.com',
+        password: 'dailydiet',
+      })
+
+      const loginResponse = await request(app.server).post('/login').send({
+        email: 'johndoe@example.com',
+        password: 'dailydiet',
+      })
+
+      const cookies = loginResponse.get('Set-Cookie')
+
+      await request(app.server).post('/meals').set('Cookie', cookies).send({
+        name: 'X-tudo',
+        description: 'X-tudo da esquina',
+        dateTime: '2023-10-17T08:30:00Z',
+        isDiet: false,
+      })
+
+      const mealsListResponse = await request(app.server)
+        .get('/meals')
+        .set('Cookie', cookies)
+
+      const mealId = mealsListResponse.body.meals[0].id
+
+      await request(app.server)
+        .put(`/meals/${mealId}`)
+        .set('Cookie', cookies)
+        .send({
+          name: 'X-bacon',
+          description: 'X-tudo da esquina',
+          dateTime: '2023-10-17T08:30:00Z',
+          isDiet: true,
+        })
+
+      const updatedMealListResponse = await request(app.server)
+        .get('/meals')
+        .set('Cookie', cookies)
+
+      expect(updatedMealListResponse.body.meals).toEqual([
+        expect.objectContaining({
+          name: 'X-bacon',
+          description: 'X-tudo da esquina',
+          meal_datetime: '2023-10-17T08:30:00Z',
+          is_diet: 1,
+        }),
+      ])
+    })
+
+    it('Should be able to delete a meal', async () => {
+      await request(app.server).post('/users').send({
+        email: 'johndoe@example.com',
+        password: 'dailydiet',
+      })
+
+      const loginResponse = await request(app.server).post('/login').send({
+        email: 'johndoe@example.com',
+        password: 'dailydiet',
+      })
+
+      const cookies = loginResponse.get('Set-Cookie')
+
+      await request(app.server).post('/meals').set('Cookie', cookies).send({
+        name: 'X-tudo',
+        description: 'X-tudo da esquina',
+        dateTime: '2023-10-17T08:30:00Z',
+        isDiet: false,
+      })
+
+      const mealsListResponse = await request(app.server)
+        .get('/meals')
+        .set('Cookie', cookies)
+
+      const mealId = mealsListResponse.body.meals[0].id
+
+      await request(app.server)
+        .delete(`/meals/${mealId}`)
+        .set('Cookie', cookies)
+
+      const updatedMealListResponse = await request(app.server)
+        .get('/meals')
+        .set('Cookie', cookies)
+
+      expect(updatedMealListResponse.body.meals).toEqual([])
     })
   })
 })
